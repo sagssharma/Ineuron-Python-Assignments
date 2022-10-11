34a5c667cb753554926759f9df148818f771a10f
sagssharma <sags.sharma@gmail.com>
2022-10-11 16:33:04 +0530

    Delete Q4.py

--- Q4.py
+++
@@ -1,7 +0,0 @@
-a=int(input("Enter 1st variable"))
-b=int(input("Enter 2nd variable"))
-print("Before Swapping a is ",a," and b is ",b)
-temp=a
-a=b
-b=temp
-print("After Swapping a is ",a," and b is ",b)
